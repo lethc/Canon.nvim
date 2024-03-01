a95bf5da55c1017ce0f74e63c8845df2525da640
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-03-01 13:54:33 -0500

    Update lualine colour

--- lua/lualine/themes/canon.lua
+++ lua/lualine/themes/canon.lua
@@ -3,8 +3,8 @@ local colors = require("canon.colors").dark
 return {
     normal = {
         a = { fg = colors.fg, bg = colors.gray08, gui = "bold" },
-        b = { fg = colors.white, bg = colors.gray01 },
-        c = { fg = colors.white, bg = colors.gray01 },
+        b = { fg = colors.white, bg = colors.alt_bg },
+        c = { fg = colors.white, bg = colors.alt_bg },
     },
     command = { a = { fg = colors.bg, bg = colors.orange, gui = "bold" } },
     insert = { a = { fg = colors.bg, bg = colors.yellow, gui = "bold" } },
