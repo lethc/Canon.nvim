96b5a19fd9a392582416920739b9b9a8081d0d93
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-03-13 15:21:23 -0500

    Update treesitter quote

--- lua/canon/init.lua
+++ lua/canon/init.lua
@@ -212,7 +212,7 @@ local set_groups = function()
         { hg = "@markup.strong.markdown_inline", fg = c.gray07, gui = "bold" },
         { hg = "@markup.strong", fg = c.gray07, gui = "bold" },
         { hg = "@markup.italic.markdown_inline", fg = c.medium_gray_blue, gui = "italic" },
-        { hg = "@markup.quote.markdown", fg = c.cyan2, bg = c.gray01, gui = "bold" },
+        { hg = "@markup.quote.markdown", fg = c.bright_white, bg = c.gray01, gui = "bold,italic" },
         { hg = "@function.macro", fg = c.cyan },
         { hg = "@type.qualifier", fg = c.white, gui = "bold" },
 
