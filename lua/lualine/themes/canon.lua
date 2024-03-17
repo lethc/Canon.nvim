2ddc43d1ca7eafc6472287a0500824a9aee333b2
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-03-17 00:03:37 -0500

    Formatting

--- lua/lualine/themes/canon.lua
+++ lua/lualine/themes/canon.lua
@@ -6,11 +6,21 @@ return {
         b = { fg = colors.white, bg = colors.alt_bg },
         c = { fg = colors.white, bg = colors.alt_bg },
     },
-    command = { a = { fg = colors.bg, bg = colors.orange, gui = "bold" } },
-    insert = { a = { fg = colors.bg, bg = colors.blue, gui = "bold" } },
-    visual = { a = { fg = colors.bg, bg = colors.yellow, gui = "bold" } },
-    terminal = { a = { fg = colors.bg, bg = colors.green, gui = "bold" } },
-    replace = { a = { fg = colors.bg, bg = colors.red, gui = "bold" } },
+    command = {
+        a = { fg = colors.bg, bg = colors.orange, gui = "bold" },
+    },
+    insert = {
+        a = { fg = colors.bg, bg = colors.blue, gui = "bold" },
+    },
+    visual = {
+        a = { fg = colors.bg, bg = colors.yellow, gui = "bold" },
+    },
+    terminal = {
+        a = { fg = colors.bg, bg = colors.green, gui = "bold" },
+    },
+    replace = {
+        a = { fg = colors.bg, bg = colors.red, gui = "bold" },
+    },
     inactive = {
         a = { fg = colors.gray05, bg = colors.bg_dark, gui = "bold" },
         b = { fg = colors.gray05, bg = colors.bg_dark },
