a0c6012edef54b63e3941ef764a02e9f608884ab
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-04-03 13:03:26 -0500

    Update LineNr colour

--- lua/canon/init.lua
+++ lua/canon/init.lua
@@ -94,7 +94,7 @@ local set_groups = function()
         { hg = "FoldColumn", fg = c.fg }, -- Left Column used to show folds
         { hg = "SignColumn" }, -- column where signs are displayed
         { hg = "IncSearch", fg = c.bright_yellow, bg = c.gray03 }, --' incsearch' highlighting; also used for the text replaced with ":s///c"
-        { hg = "LineNr", fg = c.gray04 }, -- Line number for " =number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+        { hg = "LineNr", fg = c.gray02 }, -- Line number for " =number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
         { hg = "CursorLineNr" }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
         { hg = "MatchParen", fg = c.blue, gui = "underline,bold", cterm = "underline" }, -- The character under the cursor or just before it, if it is a paired bracket, and its match.
         { hg = "ModeMsg" }, --' showmode' message (e.g., "-- INSERT --")
