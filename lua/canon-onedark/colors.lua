4758440037b6607a663841b7a2ad8a73390e008a
Eduardo Tinta <79492525+lethc@users.noreply.github.com>
2024-10-04 17:42:18 -0500

    Update

--- lua/canon-onedark/colors.lua
+++ lua/canon-onedark/colors.lua
@@ -56,62 +56,6 @@ M.dark = {
 
     special1 = "#c5c5c5",
     special2 = "#282b37",
-
-    -- Basic
-    bg = "#151515",
-    fg = "#cac9dd", --##F8F8F2
-    alt_bg = "#1b1b1b",
-    bg_dark = "#151515",
-    -- Normal
-    -- red = "#b46958",
-    -- green = "#90A959",
-    -- yellow = "#F4BF75",
-    -- orange = "#FFA557",
-    orange2 = "#E38950",
-    -- blue = "#BAD7FF",
-    blue2 = "#7580F4",
-    gray_blue = "#7E97AB",
-    medium_gray_blue = "#A2B5C1",
-    magenta = "#AA759F",
-    -- cyan = "#88afa2",
-    cyan2 = "#75F3F4",
-    cyan3 = "#ACCFC0",
-    light_gray = "#AFAFAF",
-    -- Bright
-    bright_black = "#202020",
-    bright_red = "#984936",
-    bright_green = "#586935",
-    bright_yellow = "#ab8550",
-    bright_blue = "#51657B",
-    bright_blue2 = "#576f82",
-    bright_cyan = "#f591b2",
-    bright_cyan2 = "#769d90",
-    bright_white = "#cac9dd",
-    super_bright_magenta = "#ff5555",
-    super_bright_yellow = "#F6FF55",
-    super_bright_blue = "#5563FF",
-    super_bright_green = "#55FF6E",
-    super_bright_cyan = "#55FEFF",
-    -- Grays
-    gray01 = "#202020",
-    gray02 = "#373737",
-    gray03 = "#36383F",
-    gray04 = "#727272",
-    gray05 = "#5C6276",
-    gray06 = "#9998a8",
-    gray07 = "#7E97AB",
-    gray08 = "#323232",
-    gray09 = "#1C1C1C",
-    -- Special
-    dark_red = "#201919",
-    dark_red2 = "#851D18",
-    dark_yellow = "#202019",
-    dark_blue = "#191a20",
-    dark_green = "#1c2019",
-    dark_cyan = "#192020",
-    special_magenta = "#B72A43",
-    special_red = "#6E102A",
-    none = "NONE",
 }
 
 return M
